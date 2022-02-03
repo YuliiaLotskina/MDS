83e2d103c64212e309817dc2573af9754901b7d5
YuliiaLotskina <98917548+YuliiaLotskina@users.noreply.github.com>
2022-02-03 12:10:20 +0200

    Update MDS.py

--- MDS.py
+++ MDS.py
@@ -11,20 +11,8 @@ def read():
     excel_data = pd.read_excel(path)
     print("Дані успішно прочитані")
     print(excel_data)
-    S=excel_data['Age'].values
-    """
-    i=0
-    Y=[]
-    diag="Healthy"
-    #diag="Live"
-    name='Diabetes'
-    #name='Class'
-    gf1=len(excel_data[name])
-    for i in range(gf1):
-        if excel_data[name].values[i] == diag:Y.append(1)
-        else: Y.append(0)
-        """
-    return excel_data, S
+    Y=excel_data['Age'].values
+    return excel_data, Y
 
 def TrancData(df):
     trancf=OrdinalEncoder()
@@ -54,44 +42,18 @@ def mds(n,data, inf):
 def plot2D(data_2,data_3, Y):
     fig, axes = plt.subplots(ncols=2, figsize=(14, 7))
     ax1, ax2 = axes
-    j=0
-    old1=30
-    old2=50
-    dat1=pd.DataFrame()
-    dat2=pd.DataFrame()
-    dat11=pd.DataFrame()
-    dat22=pd.DataFrame()
-    dat111=pd.DataFrame()
-    dat222=pd.DataFrame()
-    d_len=len(data_2[:,0])
-    for j in range(d_len):
-        if Y[j]<old1:
-            dat1=dat1.append(pd.DataFrame([data_2[j]]))
-            dat2=dat2.append(pd.DataFrame([data_3[j]]))
-        elif old1<=Y[j]<old2: 
-            dat11=dat11.append(pd.DataFrame([data_2[j]]))
-            dat22=dat22.append(pd.DataFrame([data_3[j]]))
-        else: 
-            dat111=dat111.append(pd.DataFrame([data_2[j]]))
-            dat222=dat222.append(pd.DataFrame([data_3[j]]))
-    size_marker=50    
-    im1 = ax1.scatter(dat1.iloc[:, 0], dat1.iloc[:, 1],s=size_marker,marker="*")
-    im1 = ax1.scatter(dat11.iloc[:, 0], dat11.iloc[:, 1],s=size_marker,marker="+")
-    im1 = ax1.scatter(dat111.iloc[:, 0], dat111.iloc[:, 1],s= size_marker,marker="s")
-    im2 = ax2.scatter(dat2.iloc[:, 0], dat2.iloc[:, 1],s= size_marker,marker="*")
-    im2 = ax2.scatter(dat22.iloc[:, 0], dat22.iloc[:, 1],s= size_marker,marker="+")
-    im2 = ax2.scatter(dat222.iloc[:, 0], dat222.iloc[:, 1],s= size_marker, marker="s")    
-    #plt.colorbar(im1, ax=ax1)
-    ax1.legend(["до 30 років","від 30 років до 50","від 50 років"],bbox_to_anchor=(1, 0), loc="center")
+    im1 = ax1.scatter(data_2[:, 0], data_2[:, 1],  c=Y,cmap='brg')
+    im2 = ax2.scatter(data_3[:, 0], data_3[:, 1],  c=Y,cmap='brg')    
+    plt.colorbar(im1, ax=ax1)
     ax1.set_title(
     'MDS(Manhattan distances)',
     fontsize=10)    
-   #plt.colorbar(im2, ax=ax2 )
-    ax2.legend(["до 30 років","від 30 років до 50","від 50 років"],bbox_to_anchor=(1, 0), loc="center")
+    plt.colorbar(im2, ax=ax2 )
     ax2.set_title(
     'MDS(Euclidean distances)',
-    fontsize=10 )    
+    fontsize=10, )    
     plt.show()
+
     
 def NStress(data,inf,m):
     stress = []
@@ -135,39 +97,19 @@ def plotStress(max_range,stress):
     plt.title('Стрес MDS')
     plt.ylabel('стрес')
     plt.legend(('Manhattan distances'))
-    plt.savefig("MDS 2D.png")
     plt.show()
     
    
-def plot3D(data_2, Y,title):    
-    ax1 = plt.subplot (projection = '3d')  
-    j=0
-    old1=30
-    old2=50
-    dat1=pd.DataFrame()
-    dat2=pd.DataFrame()
-    dat3=pd.DataFrame()
-    d_len=len(data_2[:,0])
-    for j in range(d_len):
-        if Y[j]<old1:
-            dat1=dat1.append(pd.DataFrame([data_2[j]]))
-        elif old1<=Y[j]<old2: 
-            dat2=dat2.append(pd.DataFrame([data_2[j]]))
-        else: 
-            dat3=dat3.append(pd.DataFrame([data_2[j]]))
-            
-    size_marker=30    
-    im1 = ax1.scatter(dat1.iloc[:, 0], dat1.iloc[:, 1],s=size_marker,marker="+")
-    im1 = ax1.scatter(dat2.iloc[:, 0], dat2.iloc[:, 1],s=size_marker,marker="*")
-    im1 = ax1.scatter(dat3.iloc[:, 0], dat3.iloc[:, 1],s= size_marker,marker="s")
-    #ax1=ax.scatter (data_2[:, 0], data_2[:, 1], data_2[:,2], c=Y,cmap='brg') 
-    #plt.colorbar(ax1)
-    ax1.legend(["до 30 років","від 30 років до 50","від 50 років"],bbox_to_anchor=(0, 0), loc="center")
-    ax1.set_title(
+
+ def plot3D(data_2, Y,title):    
+    ax = plt.subplot (projection = '3d')  
+    ax1=ax.scatter (data_2[:, 0], data_2[:, 1], data_2[:,2], c=Y,cmap='brg') 
+    plt.colorbar(ax1)
+    ax.set_title(
     title,
     fontsize=10)
     plt.show()
-    plt.savefig("MDS 3D.png")
+
 def plotMatrix(D1,D2):
     fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
     ax1, ax2 = axes
@@ -184,19 +126,19 @@ def plotMatrix(D1,D2):
     plt.show()
     
 def save(data,data1,inf,inf2):
-    #data=([data_1[:,0],data_1[:,1],data_2[:,0],data_2[:,1]])
     df = pd.DataFrame(data)
     df1= pd.DataFrame(data1)
     with pd.ExcelWriter('./MDS2.xlsx') as writer:  # doctest: +SKIP
      df.to_excel(writer, sheet_name=inf)
      df1.to_excel(writer, sheet_name=inf2)
     print("Дані збережено в файлі 'MDS1.xlsx' ")
+    
 def main():
     df, Y=read()
     data=TrancData(df) 
     n=int(input("Введіть розмірність нового набору даних:\n"))
-    #m_mds=my_mds(data,n)
     g_mds, D1,S1=mds(n,data, 1)
+    
     eu_mds,D2,S2=mds(n,data, 2)
     print("Матриця (Manhattan MDS):\n",D1)
     print("Матриця (Euclidean MDS):\n",D2)
@@ -210,14 +152,9 @@ def main():
         plot3D(g_mds,Y,'MDS Manhattan distances')
         plot3D(eu_mds,Y,'MDS Euclidean distances')
     else: print("Для такої розмірності даних графічне відображення не реалізовано")
-    #save(g_mds,eu_mds,"Manhattan","Euclidean")
-     
+    
     #plotStress(21,NStress(data,2,20))
-    
-    #save_rezult(m_mds, eu_mds, manh_mds)    
-    
-
-    
+       
 
 if __name__ == '__main__':
     main()
